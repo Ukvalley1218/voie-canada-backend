7a5f979d4d1efb9b126c229b73523906b612a0ef
Santosh971 <santoshshimpankar61@gmail.com>
2026-05-21 13:54:09 +0530

    Backend changes

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174','http://localhost:5175', 'http://localhost:3000', 'https://voie-canada-backend.onrender.com','https://voiecanada.b100x.in','https://adminvoie.b100x.in'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:3000', 'https://voie-canada-backend.onrender.com', 'https://voiecanada.b100x.in', 'https://adminvoie.b100x.in'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
